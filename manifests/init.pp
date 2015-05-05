6640e523d7c2c44d3504bf39d1a2280f7d3a9554
Patrick Tully <patrick@avatarnewyork.com>
2015-05-05 18:06:00 +0000

    adding drush package

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,24 +30,10 @@ class drush {
         provider => pear,
         require  => Package['php-pear'],
     }
-
-    exec { 'fetch-drush':
-        cwd     => '/tmp',
-        command => "/usr/bin/git clone --branch $drush::params::branch_name http://git.drupal.org/project/drush.git",
-        creates => '/tmp/drush',
-        require => Package['php5-cli', 'php-pear', 'Console_Table', 'git'], 
+    package { 'drush':
+        ensure   => present,
+        provider => pear,
+        require  => Package['Console_Table'],
     }
 
-    file { '/usr/local/lib/drush':
-        ensure  => directory,
-        recurse => true,
-        purge   => true,
-        source  => '/tmp/drush',
-        require => Exec['fetch-drush'],
-    }
-
-    file { '/usr/local/bin/drush':
-        ensure  => '/usr/local/lib/drush/drush',
-        require => File['/usr/local/lib/drush'],
-    }
 }
